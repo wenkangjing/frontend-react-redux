579d7d87816aeb9993034df469b6c74afd0f6a1a
kwen <wenkangjing@gmail.com>
2017-08-11 11:01:39 +1000

    refresh weather app

---
+++ udemy/_middleware.md
@@ -0,0 +1,4 @@
+Intermediate Redux: Middleware
+------------------------------
+
+
--- udemy/weather/src/index.js
+++ udemy/weather/src/index.js
@@ -1,17 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import ReduxPromise from "redux-promise";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-import App from "./components/app";
-import reducers from "./reducers";
+import App from './components/app';
+import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <App />
-  </Provider>,
-  document.querySelector(".container")
-);
+  </Provider>
+  , document.querySelector('.container'));
