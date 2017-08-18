ab0969c1ead924b782744b41b300451061443652
kwen <wenkangjing@gmail.com>
2017-08-18 17:12:08 +1000

    Sparkline Charts

--- udemy/README.md
+++ udemy/README.md
@@ -3,9 +3,9 @@ Modern React with Redux
 
 [Course](https://www.udemy.com/react-redux/learn/v4/t/lecture/4224604?start=0)
 
-* [x] [React](./_react.md)
-* [x] [Redux](./_redux.md)
-* [ ] Redux Middleware
+* [x] [React](./1_react.md)
+* [x] [Redux](./2_redux.md)
+* [ ] [Redux Middleware](3_middleware.md)
 * [ ] React Router + Redux Form
 * [ ] RallyCoding
 
--- udemy/weather/src/containers/weather_list.js
+++ udemy/weather/src/containers/weather_list.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine, SparklinesSpots } from 'react-sparklines'; 
 
 class WeatherList extends Component {
   renderWeather(cityData) {
+    const temps = cityData.list.map(weather => weather.main.temp);
+    const pressure = cityData.list.map(weather => weather.main.pressure);
+    const humidity = cityData.list.map(weather => weather.main.humidity);
+
     return (
       <tr key={cityData.city.id}>
         <td>{cityData.city.name}</td>
+        <td>
+          <Sparklines data={temps}>
+            <SparklinesLine />
+            <SparklinesSpots />
+          </Sparklines>
+        </td>
+        <td>
+          <Sparklines data={pressure}>
+            <SparklinesLine />
+            <SparklinesSpots />
+          </Sparklines>
+        </td>
+        <td>
+          <Sparklines data={humidity}>
+            <SparklinesLine />
+            <SparklinesSpots />
+          </Sparklines>
+        </td>
       </tr>
     );
   }
+
   render() {
     return (
       <table className="table table-hover">
